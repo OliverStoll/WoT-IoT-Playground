b484a97891b3f2079cab5bbe9e0dd6e26774ea5f
F2499R <f.risling@campus.tu-berlin.de>
2023-04-29 19:03:57 +0200

    add backend /api/config

---
+++ backend/index.ts
@@ -0,0 +1,27 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello from the backend!');
+});
+
+app.post('/api/config', (req, res) => {
+    const { config } = req.body;
+    // Hier kannst du den config-JSON-String weiterverarbeiten
+    console.log(config);
+    res.send('Config was processed!');
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+});
