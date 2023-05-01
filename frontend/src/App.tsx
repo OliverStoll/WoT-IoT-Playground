c243f530bb5643e5cbb518cc22c6c2778589040a
Marc Niemella <mfniemella@gmail.com>
2023-05-01 20:23:33 +0200

    refactoring

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,47 +1,16 @@
 import Heading from "./components/Heading.tsx";
 import {Section} from "./components/Section.tsx";
-import FileUpload from "./components/FileUpload.tsx";
+import {FileUpload, handleFilesAdded}  from "./components/FileUpload.tsx";
 
-const url = 'http://localhost:5000/api/config';
 
 function App() {
     return (
         <>
-            <Heading title={"WoT Playground"}></Heading>
+            <Heading title={"WoT Playground"}/>
             <Section title={"Simulation via Configuration"}>Upload json config</Section>
             <FileUpload allowedFileTypes={['application/json']} onFilesAdded={handleFilesAdded}/>
         </>
     )
-}
-
-function handleFilesAdded(files: File[]) {
-    files[0].text().then(function(json) {
-        if (validateJson(json)){
-            console.log("Sending JSON to backend")
-            console.log(json)
-            console.log(sendPostRequest(url, json));
-
-        } else console.log("ERROR: Not a valid JSON")
-    });
-}
-
-function validateJson (json: string){
-    return true
-}
-
-async function sendPostRequest(url: string, data: string) {
-    try {
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: data
-        });
-        const responseData = await response;
-        console.log(responseData);
-        return responseData;
-    } catch (error) {
-        console.error(error);
-    }
 }
 
 
--- frontend/src/components/FileUpload.tsx
+++ frontend/src/components/FileUpload.tsx
@@ -1,47 +1,84 @@
 import { useCallback } from "react";
 import { useDropzone } from "react-dropzone";
-import './ComponentStyle.css';
+import './ComponentStyle.css'
+
+const url: string = 'http://localhost:5000/api/config'
 
 type FileUploadProps = {
-    onFilesAdded: (files: File[]) => void;
-    disabled?: boolean;
-    allowedFileTypes?: string[];
+    onFilesAdded: (files: File[]) => void
+    allowedFileTypes?: string[]
 };
 
 function FileUpload(props: FileUploadProps) {
-    const { onFilesAdded, disabled, allowedFileTypes } = props;
+    const { onFilesAdded, allowedFileTypes } = props
 
-    const handleDrop = useCallback(
+    const onDrop = useCallback(
         (acceptedFiles: File[]) => {
             const filteredFiles = allowedFileTypes
                 ? acceptedFiles.filter((file) =>
                     allowedFileTypes.includes(file.type)
                 )
-                : acceptedFiles;
+                : acceptedFiles
 
-            onFilesAdded(filteredFiles);
+            onFilesAdded(filteredFiles)
         },
         [allowedFileTypes, onFilesAdded]
-    );
+    )
 
-    const { getRootProps, getInputProps, isDragActive } = useDropzone({
-        onDrop: handleDrop,
-        disabled,
-    });
+    const { getRootProps, getInputProps} = useDropzone({
+        multiple: false,
+        onDragEnter: undefined,
+        onDragLeave: undefined,
+        onDragOver: undefined,
+        onDrop})
 
 
     return (
         <div {...getRootProps()} className="file-upload-container">
             <input {...getInputProps()} />
             <p>
-                {isDragActive
-                    ? `Ziehen Sie die ${allowedFileTypes?.join(" oder ")} Dateien hierhin.`
-                    : `Klicken Sie hier oder ziehen Sie ${allowedFileTypes?.join(
-                        " oder "
-                    )} Dateien hierhin.`}
+                Drag 'n' drop a {allowedFileTypes?.join(" or ")} file here, or click to select file
             </p>
         </div>
-    );
+    )
 }
 
-export default FileUpload;
+function handleFilesAdded(files: File[]) {
+    console.log(files[0])
+    if (typeof files[0] !== "undefined") {
+        files[0].text().then(function (json) {
+            if (validateJson(json)) {
+                console.log("Sending JSON to backend!")
+                console.log(json)
+                sendPostRequest(url, json).then(r => console.log(r))
+            } else console.log("ERROR: Not a valid JSON!")
+        })
+    } else console.log("ERROR: Not a valid JSON!")
+}
+
+function validateJson (json: string){
+    try {
+        let o = JSON.parse(json)
+        if (o && typeof o === "object" && o !== "") {
+            return true
+        }
+    }
+    catch {}
+    return false
+}
+
+async function sendPostRequest(url: string, data: string) {
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: data
+        })
+        if (response.ok) return response.text()
+        else console.log("Error: The server could not handle the request, please try again!")
+    } catch {
+        console.log("Error: The server could not handle the request, please try again!")
+    }
+}
+
+export {FileUpload, handleFilesAdded}
