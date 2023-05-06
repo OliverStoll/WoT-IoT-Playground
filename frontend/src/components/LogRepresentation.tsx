59a48ccc2fb2eb3f31429040bcabb6a5f4b8915c
F2499R <f.risling@campus.tu-berlin.de>
2023-05-06 15:44:31 +0200

    add empty components

---
+++ frontend/src/components/LogRepresentation.tsx
@@ -0,0 +1,6 @@
+const LogRepresentation = ()  => {
+    return (
+        <h1>LogRepresentation</h1>
+    )
+}
+export default LogRepresentation
---
+++ frontend/src/components/ThingRepresentation.tsx
@@ -0,0 +1,6 @@
+const ThingRepresentation = ()  => {
+    return (
+        <h1>ThingRepresentation</h1>
+    )
+}
+export default ThingRepresentation
