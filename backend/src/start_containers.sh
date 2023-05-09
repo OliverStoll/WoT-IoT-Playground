c7536689a2bfed970810646cae641078531eb191
OliverStoll <oliverstoll.berlin@gmail.com>
2023-05-09 13:08:25 +0200

    Added starting script for containers and a proper config.json which includes basic logic options

--- backend/src/start_containers.sh
+++ backend/src/start_containers.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-
 # script that extracts the number of devices from scenario and runs a docker container for each device with an increasing id
 
 # get the num devices from config.yml with shyaml
 # num_devices=$(cat config.yml | shyaml get-length devices)
 
 # get the num devices from config.json with jq
-json_file="config.json"
+json_file="../config.json"
 num_devices=$(jq '.devices | length' $json_file)
 echo "Devices: $num_devices"
 
--- wot-blueprint/wot_device.ts
+++ wot-blueprint/wot_device.ts
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 const paths = ['/temperature', '/state']; // Replace this with your list of paths
@@ -9,7 +10,15 @@ for (const path of paths) {
   });
 }
 
-const PORT = 3000; // Replace with the port you want to use
+// get the environment variable for the port and convert to number
+const PORT = Number(process.env.PORT) || 3000;
+const DEVICE_IDX = Number(process.env.DEVICE_IDX) || 0;
+
+// get the device from the scenario
+const scenario = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+const device = scenario.devices[DEVICE_IDX];
+console.log(device);
+
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
