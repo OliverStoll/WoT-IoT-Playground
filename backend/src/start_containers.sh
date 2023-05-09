a401dcebbc0f1a96e43e9f58b3dc52d0a222e37e
OliverStoll <oliverstoll.berlin@gmail.com>
2023-05-09 07:24:56 +0200

    Add container starting script

--- backend/src/start_containers.sh
+++ backend/src/start_containers.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-json_file="config.json"
-echo "Parsing config"
-echo "Building images"
-echo "Starting containers"
+# script that extracts the number of devices from scenario and runs a docker container for each device with an increasing id
 
-#TODO Ersetzen mit richtigem Script
-docker run busybox
+# get the num devices from config.yml with shyaml
+# num_devices=$(cat config.yml | shyaml get-length devices)
+
+# get the num devices from config.json with jq
+json_file="config.json"
+num_devices=$(jq '.devices | length' $json_file)
+echo "Devices: $num_devices"
+
+for (( i=0; i<$num_devices; i++ ))
+do
+  echo "Running docker id $i"
+  # execute docker run command detached with port mapping and environment variable
+  docker run -d -p 300$i:300$i -e PORT=300$i wot-device
+done
+
+
+sleep 10
+
