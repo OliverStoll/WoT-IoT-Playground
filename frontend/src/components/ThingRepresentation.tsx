029897ada43f0788a0d3a3a5607cfabcd1dac85b
mf189 <niemella@campus.tu-berlin.de>
2023-05-20 13:19:10 +0200

    dynamically change displayed things based on the TD + refactoring + comments

--- frontend/src/components/ThingRepresentation.tsx
+++ frontend/src/components/ThingRepresentation.tsx
@@ -2,16 +2,22 @@ import {useEffect, useState} from "react";
 
 const url: string = 'http://localhost:5000/api/logs/thingDescriptions'
 
+
+/**
+ Renders a component that fetches and displays a list of thing representations.
+ The component periodically fetches thing descriptions and updates the list if there are changes.
+ @returns {JSX.Element} The rendered component displaying the thing representations.
+ */
 const ThingRepresentation = () => {
     const [things, setThings] = useState<JSX.Element[]>([])
-    let confString: string
+    let oldConf: string
     useEffect(() => {
-        const interval = setInterval(function () {
+        const interval: number = setInterval(function () {
             fetchThingDescriptions().then(function (res: string) {
-                if (res === "Error" || res === "No config file found" || res === confString) return
-                confString = res
-                let conf: object = JSON.parse(res)
-                setThings(getThings(conf))
+                // if an error occurred, the list is empty or nothing has changed -> return
+                if (res === "Error" || res === "[]" || res === oldConf) return
+                oldConf = res
+                setThings(getThings(JSON.parse(res)))
             })
         }, 5000);
         return () => {
@@ -21,28 +27,38 @@ const ThingRepresentation = () => {
     return <div>{things}</div>
 }
 
-function getThings(conf) {
-    let thing_keys: string[] = Object.keys(conf)
-    let things: JSX.Element[] = []
-    try {
-        things = Array.from({length: thing_keys.length}, function (_, index: number) {
-            let att_keys: string[] = Object.keys(conf[thing_keys[index]])
-            let attributes: JSX.Element[] = Array.from({length: att_keys.length}, (_, ind: number) => (
-                <div key={conf[thing_keys[index]]["id"] + "-" + att_keys[ind]}>
-                    {att_keys[ind]}: {conf[thing_keys[index]][att_keys[ind]]}
-                </div>
-            ))
-            return (
-                <div key={conf[thing_keys[index]]["id"]}>
-                    <img src="../../resources/wot_icon.png" alt="Thing icon" height={"100"}/>
-                    <div>{attributes}</div>
-                </div>
-            )
-        })
-    }catch (error) {things = [<div>Wrong Format</div>]}
-    return things
+/**
+ Converts a list of thing configurations into an array of JSX elements representing each thing.
+ Each thing element includes an icon, along with selected attributes of the thing.
+ @param {string[]} conf - The list of thing configurations.
+ @returns {JSX.Element[]} An array of JSX elements representing the things.
+ */
+function getThings(conf: string[]): JSX.Element[] {
+    return Array.from({length: conf.length}, function (_, index: number) {
+        let thing = JSON.parse(conf[index])
+        // important attributes you want to show
+        let attToShow: string[] = ["id", "title", "actions", "properties"]
+        let att_keys: string[] = Object.keys(thing)
+            .filter(function (key: string){ return attToShow.includes(key)})
+        //create a div for the important attributes of every thing description
+        let attributes: JSX.Element[] = Array.from({length: att_keys.length}, (_, ind: number) => (
+            <div key={thing["id"] + "-" + att_keys[ind]}>
+                {att_keys[ind]}: {JSON.stringify(thing[att_keys[ind]]).replaceAll("\"", "")}
+            </div>))
+        // create div for every thing description with a symbol and all attributes
+        return (
+            <div key={thing["id"]}>
+                <img src="../../resources/wot_icon.png" alt="Thing icon" height={"100"}/>
+                <div>{attributes}</div>
+            </div>
+        )
+    })
 }
 
+/**
+ Fetches thing descriptions from a specified URL and returns them as a string.
+ @returns {Promise<string>} A promise that resolves to the fetched thing descriptions as a string.
+ */
 async function fetchThingDescriptions(): Promise <string> {
     try {
         const response: Response = await fetch(url);
