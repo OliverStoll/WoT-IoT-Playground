337b7150c9ffe12375c91e7b95435b348a1268da
Marc Niemella <niemella@campus.tu-berlin.de>
2023-06-23 10:47:41 +0200

    fix docker for windows

--- backend/src/start_containers.sh
+++ backend/src/start_containers.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 # script that extracts the number of devices from scenario and runs a docker container for each device with an increasing id
-
 # get the num devices from config.json with jq
 # local
 #json_file="../wot-blueprint/config.json"
 # docker
 json_file="../../wot-blueprint/config.json"
-
 echo $(pwd)
 num_devices=$(jq '.devices | length' $json_file)
 echo "Devices: $num_devices"
-
 # Check if the wot-device image is locally available
 if ! docker image inspect wot-device &>/dev/null; then
   echo "wot-device image not found locally. Building the image..."
@@ -18,14 +15,11 @@ if ! docker image inspect wot-device &>/dev/null; then
   docker build -t wot-device . &>/dev/null;
   cd -
 fi
-
-for (( i=0; i<$num_devices; i++ ))
+for (( i=0; i<num_devices; i++ ))
 do
   echo "Running docker id $i"
-  port=$((3000 + $i))
-
+  port=$((3000 + i))
   # execute docker run command detached with port mapping and environment variable
   docker run -d -p $port:$port -e PORT=$port -e DEVICE_IDX=$i wot-device
 done
-
 sleep 10
