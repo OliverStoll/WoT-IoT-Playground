b9390ec7e8e3216a764051465c033e581e00044f
F2499R <f.risling@campus.tu-berlin.de>
2023-07-04 17:39:58 +0200

    add calling of external device

--- backend/src/utils/sendRequest.ts
+++ backend/src/utils/sendRequest.ts
@@ -5,14 +5,15 @@ async function sendRequest(data) {
     // handler for http wot devices
     const { href } = data
     const method = data['htv:methodName']
-
+    let response: string = ""
     if(href.split('://')[0]=='http'){
         const protocol: ProtocolInterface = new HttpProtocol(this, 5001)
-        if(!method){
-            return await protocol.receive(href)
+        if(!method || method == "GET"){
+            response = await protocol.receive(href)
+
         }
         else{
-            return protocol.send(href, data);
+            protocol.send(href, data);
         }
 
     }
@@ -20,6 +21,17 @@ async function sendRequest(data) {
     else{
         return "Error: Only Http implemented. Add more protocols by extending the ProtocolInterface"
     }
+
+    // check if response object is valid json to avoid backend crashes
+    try{
+        JSON.parse(response)
+    }
+    catch (e) {
+        console.log(`Response for Request to URL: ${href} is not valid JSON!`)
+        response = ""
+    }
+
+    return response
 }
 
 module.exports = sendRequest;
