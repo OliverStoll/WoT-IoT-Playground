22bd5a0663c07dd7b626623b476d5f70308134eb
Marc Niemella <niemella@campus.tu-berlin.de>
2023-07-08 12:52:33 +0200

    fix script for windows

--- backend/src/start_containers.sh
+++ backend/src/start_containers.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 # script that extracts the number of devices from scenario and runs a docker container for each device with an increasing id
-
 # get the num devices from config_backup.json with jq
 json_file="../device-blueprint/mount_volume/scenario.json"
-
 # echo if the file exists
 if [ -f "$json_file" ]; then
     echo "Scenario File exists."
@@ -11,22 +9,18 @@ else
     echo "Scenario File does not exist."
     exit 1
 fi
-
 echo "$(pwd)"
 num_devices=$(jq '.devices | length' $json_file)
 echo "Devices: $num_devices"
-
 # Check if the wot-device image is locally available
 echo "Building the image..."
 cd ../device-blueprint || exit
 docker build -t wot-device .
 cd - || exit
-
 for (( i=0; i< num_devices; i++ ))
 do
   device_title=$(jq --raw-output ".devices[$i].title" $json_file)
   device_name=$(echo "$device_title" | tr ' ' '_' | tr '[:upper:]' '[:lower:]')
-
   port=$((3000 + i))
   # execute docker run command detached with port mapping and environment variable
   docker run -d -p $port:$port --name "$device_name" -e PORT=$port -e DEVICE_IDX=$i --network="web-of-things-playground_default" wot-device
