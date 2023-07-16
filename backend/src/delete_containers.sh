651d2f81ee445fbdcbeca0fc45f93113ee78b6cf
Marc Niemella <niemella@campus.tu-berlin.de>
2023-07-16 13:49:27 +0200

    hopefully the last script change for windows...

--- backend/src/delete_containers.sh
+++ backend/src/delete_containers.sh
@@ -4,5 +4,5 @@
 docker ps -aqf "ancestor=wot-device" | xargs -r docker stop >/dev/null 2>&1
 docker ps -aqf "ancestor=wot-device" | xargs -r docker rm >/dev/null 2>&1
 # Delete the Docker image 'wot-device'
-docker image rm wot-device >/dev/null 2>&1
+docker image rmi wot-device >/dev/null 2>&1
 echo "All containers with the image 'wot-device' have been stopped and removed."
